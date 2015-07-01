f17d9c6fb0a0464fc87ba3d96f8ab1524d0683ab
Kim Silkebækken <kim@silkebaekken.no>
2015-07-01 20:38:39 +0200

    Add atmosphere details to resources

--- app/assets/js/resources/bodies.js
+++ app/assets/js/resources/bodies.js
@@ -4,6 +4,8 @@ export var bodies = {
 		radius: 600000,
 		atmosphere: 70000,
 		rotPeriod: 21600,
+		atmosphereColor: 0x0077cc,
+		atmosphereOpacity: 0.2,
 		textures: {
 			diffuse: require('../../../assets/img/maps/kerbin-diffuse.jpg'),
 			specular: require('../../../assets/img/maps/kerbin-specular.png'),
