060c1ad83e87445e7ed73c9ee9a70005dc38d976
Kim Silkebækken <kim@silkebaekken.no>
2015-07-03 16:17:24 +0200

    Add more info to map screen

--- app/assets/js/resources/bodies.js
+++ app/assets/js/resources/bodies.js
@@ -1,12 +1,19 @@
 export var bodies = {
 	_atmDensity: 1.2230948554874,
+
 	Kerbin: {
 		name: 'Kerbin',
 		radius: 600000,
-		atmosphere: 70000,
 		rotPeriod: 21600,
+		atmosphere: 70000,
+		atmosphereScaleHeight: 5600,
 		atmosphereColor: 0x0077cc,
 		atmosphereOpacity: 0.2,
+		info: {
+			surfaceGravity: 9.81,
+			escapeVelocity: 3431.03,
+			soi: 84159286,
+		},
 		textures: {
 			diffuse: require('../../../assets/img/maps/kerbin-diffuse.jpg'),
 			specular: require('../../../assets/img/maps/kerbin-specular.png'),
